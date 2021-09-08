250c9be99bea054978d3abd1571efd81f6f0d9ea
joshuapohan-kargo <joshua.pohan@kargo.tech>
2021-09-08 16:25:34 +0700

    add comment

---
+++ lib/medium_graphql_api/blog/post.ex
@@ -0,0 +1,22 @@
+defmodule MediumGraphqlApi.Blog.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias MediumGraphqlApi.Accounts.User
+
+  schema "posts" do
+    field :content, :string
+    field :published, :boolean, default: false
+    field :title, :string
+    belongs_to(:user, User)
+
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :content, :published, :user_id])
+    |> validate_required([:title, :content, :published, :user_id])
+  end
+end
---
+++ lib/medium_graphql_api_web/resolvers/post_resolver.ex
@@ -0,0 +1,13 @@
+defmodule MediumGraphqlApiWeb.Resolvers.PostResolver do
+
+    alias MediumGraphqlApi.Blog
+
+    def posts(_, _, _) do
+        {:ok, Blog.list_posts()}
+    end
+
+    def create_post(_, %{input: input} , %{context: %{current_user: current_user}}) do
+        post_input = Map.merge(input,  %{user_id: current_user.id})
+        Blog.create_post(post_input)
+    end
+end
